74b98e7073b892b25fe8b657697cf067f8e9a5e6
Alawode Samuel <tolulope.alawode@gmail.com>
2023-09-25 14:35:55 +0100

    lets go

---
+++ src/main/java/com/jojo/GitTrack/GitTrackApplication.java
@@ -0,0 +1,13 @@
+package com.jojo.GitTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jojo/GitTrack/GitTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojo.GitTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
